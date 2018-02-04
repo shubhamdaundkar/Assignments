c1a004f0971423efd489585b39cdfe64f80664b4
Shubham <shubham.daundkar@wgblindia.com>
2018-02-04 15:31:56 +0530

    added & updated script also class_management_systems tables

--- ssh_scripts.sh
+++ ssh_scripts.sh
@@ -2,6 +2,7 @@
 echo "Enter name of dir":
 read filename
 mkdir -p $filename
+mkdir -p $filename1 | rev
 
 a=1
 while [ $a -lt 4 ]
@@ -10,5 +11,5 @@ do
   echo "some data for $filename${a}">> $filename/$filename${a}.txt
    a=`expr $a + 1`
  done
-
 zip -r $filename $filename
+gzip -d $filename1.zip
